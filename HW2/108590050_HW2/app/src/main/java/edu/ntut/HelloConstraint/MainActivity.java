77beb4490bf5f893d31a69d4288e6957e0be6f00
Ho-Ming Li <ming2007119@gmail.com>
2021-03-02 17:40:20 +0800

    HW2

---
+++ HW2/108590050_HW2/app/src/main/java/edu/ntut/HelloConstraint/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.ntut.HelloConstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    private Button mShowColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        mShowColor = (Button) findViewById(R.id.button_zero);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+            mShowColor.setBackgroundColor(Color.RED);
+            if (mCount%2 == 0) {
+                view.setBackgroundColor(Color.MAGENTA);
+            } else {
+                view.setBackgroundColor(Color.GREEN);
+            }
+        }
+    }
+
+    public void setZero(View view) {
+        mCount = 0;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+            mShowColor.setBackgroundColor(Color.GRAY);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ### [HW1](https://github.com/Ming119/109-2_Android-Application-Development/tree/master/HW1)
 
+### [HW2](https://github.com/Ming119/109-2_Android-Application-Development/tree/master/HW2)
+
 ## LICENSE
 [GNU General Public License v3.0](https://github.com/Ming119/109-2_Android-Application-Development/blob/master/LICENSE)
