7eabbbd282887cd44ca414734f28b1fe6053e29a
Ho-Ming Li <ming2007119@gmail.com>
2021-03-16 17:22:44 +0800

    Update HW4

---
+++ HW4/2.2/108590050_HW4 2.2/app/src/main/java/edu/ntut/a108590050_hw2_2/MainActivity.java
@@ -0,0 +1,85 @@
+package edu.ntut.a108590050_hw2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView textCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreate()", "onCreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textCount = (TextView) findViewById(R.id.text_count);
+
+        if (savedInstanceState != null) {
+            String saveCount = savedInstanceState.getString("count");
+            count = Integer.parseInt(saveCount);
+            if (textCount != null) {
+                textCount.setText(saveCount);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("onStart()", "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("onRestart()", "onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("onResume()", "onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("onPause()", "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("onStop()", "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("onDestroy()", "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d("onSaveInstanceState()", "onSaveInstanceState()");
+        super.onSaveInstanceState(outState);
+        outState.putString("count", String.valueOf(textCount.getText()));
+    }
+
+    public void clickHandler_count(View buttonCount) {
+        count ++;
+
+        if (textCount != null) {
+            textCount.setText(Integer.toString(count));
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # 109-2 Android Application Development
 
-### [HW1](https://github.com/Ming119/109-2_Android-Application-Development/tree/master/HW1)
+### [HW1](./HW1)
 
-### [HW2](https://github.com/Ming119/109-2_Android-Application-Development/tree/master/HW2)
+### [HW2](./HW2)
 
-### [HW3](https://github.com/Ming119/109-2_Android-Application-Development/tree/master/HW3)
+### [HW3](./HW3)
+
+### [HW4](./HW4)
+
 
 ## LICENSE
 [GNU General Public License v3.0](https://github.com/Ming119/109-2_Android-Application-Development/blob/master/LICENSE)
